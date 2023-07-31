1e2e492acea2ee4c987eb3e4e79c1b5fd059c81b
Nicholas Boshart <me@nichabosh.com>
2023-07-30 23:25:24 -0700

    feat(database): create user document model & construction method

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/nichabosh/amionline
 
 go 1.20
 
-require go.mongodb.org/mongo-driver v1.12.0
+require (
+	go.mongodb.org/mongo-driver v1.12.0
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
+)
 
 require (
 	github.com/golang/snappy v0.0.1 // indirect
@@ -12,7 +15,6 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
---
+++ mongodb/models/user.go
@@ -0,0 +1,52 @@
+package schemas
+
+import (
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	uuid      primitive.ObjectID `bson:"_id,omitempty"`
+	createdAt primitive.DateTime `bson:"createdAt,omitempty"`
+	updatedAt primitive.DateTime `bson:"updatedAt,omitempty"`
+
+	firstName string `bson:"firstName" required:"true"`
+	lastName  string `bson:"lastName" required:"true"`
+	username  string `bson:"username" required:"true"`
+	email     string `bson:"email" required:"true"`
+	password  string `bson:"password" required:"true"`
+}
+
+// NewUser takes in a set of user-provided credentials (i.e. first and
+// last name, username, email, password), instantiates a new User object
+// with unique identifiers and timestamp information, and hashes/salts the
+// passed-in, plaintext password to ensure secure storage in the database.
+func NewUser(fname, lname, uname, email, pass string) (*User, error) {
+	uuid := primitive.NewObjectID()
+	now := primitive.NewDateTimeFromTime(time.Now())
+	if err := hashAndSaltPassword(&pass); err != nil {
+		return nil, fmt.Errorf("cannot hash password: %w", err)
+	}
+
+	return &User{
+		uuid:      uuid,
+		createdAt: now,
+		updatedAt: now,
+
+		firstName: fname,
+		lastName:  lname,
+		username:  uname,
+		email:     email,
+		password:  pass,
+	}, nil
+}
+
+func hashAndSaltPassword(password *string) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword(
+		[]byte(*password), bcrypt.DefaultCost)
+	*password = string(hashedBytes)
+	return err
+}
